4e6b1b8dbc81e20c0e8894e870e79e5e9de5b4fe
babanamngua <93252539+babanamngua@users.noreply.github.com>
2021-11-15 17:57:22 +0700

    Add files via upload

---
+++ xlThem.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Siêu thị điện máy</title>
+
+<style type="text/css">
+.title {
+	font-weight: bold;
+	color: #FFF;
+}
+.oidoioi {
+border: 1px solid #6da6b1;
+padding: 2px;
+}
+.boiden{
+  font-weight: bold;
+  font-size: 17px;
+  padding: 3px;
+}
+.boixanh{
+  color:rgb(8, 160, 145);
+  padding: 3px;
+}
+</style>
+</head>
+
+<body>
+<table width="1000" align="center" border="0" cellpadding="0" cellspacing="0">
+	<tr><!-- logo và banner -->
+    	<td background="images/layout/header_bg.jpg">
+        	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+            	<tr>
+                	<td height="173" width="300" align="center" valign="middle">
+                    	<img src="images/layout/logo.png" width="284" height="160" />
+                    </td>
+                    <td width="700" align="center">
+                    	<img src="images/layout/banner.png" width="600" height="173" />
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr><!-- menu -->
+    	<td>
+        	<table width="100%" border="0" cellspacing="0" cellpadding="0" >
+              <tr>
+                <td height="50" width="9" background="images/layout/menu_left.gif">&nbsp;</td>
+                <td background="images/layout/menu_bg.gif" width="190" align="center"><a href="index.html" style="text-decoration:none;color:#676d77"><img src="images/layout/home.png"/>lab4_bài 1</a></td>
+                <td width="2"><img src="images/layout/menu_divider.gif" width="2" height="50" /></td>
+                <td background="images/layout/menu_bg.gif" width="190" align="center"><a href="dichvu.html" style="text-decoration:none;color:#676d77"><img src="images/layout/services.png"/>lab4_bài 2</a></td>
+                <td width="2"><img src="images/layout/menu_divider.gif" width="2" height="50" /></td>
+                <td background="images/layout/menu_bg.gif" width="190" align="center"><a href="#" style="text-decoration:none;color:#676d77"><img src="images/layout/favs.png"/>Tin khuyến mãi</a></td>
+                <td width="2"><img src="images/layout/menu_divider.gif" width="2" height="50" /></td>
+                <td background="images/layout/menu_bg.gif" width="190" align="center"><a href="#" style="text-decoration:none;color:#676d77"><img src="images/layout/car.png"/>Giao hàng</a></td>
+                <td width="2"><img src="images/layout/menu_divider.gif" width="2" height="50" /></td>
+                <td background="images/layout/menu_bg.gif" width="190" align="center"><a href="#" style="text-decoration:none;color:#676d77"><img src="images/layout/user_add.png"/>Đăng nhập</a></td>
+                <td height="50" width="9" background="images/layout/menu_right.gif">&nbsp;</td>
+              </tr>
+</table>
+
+        </td>
+    </tr>
+    <tr><!-- dòng trắng -->
+    	<td height="10"></td>
+    </tr>
+    <tr> <!-- Nội dung -->
+    	<td>
+        	<table width="100%" border="0" cellpadding="0" cellspacing="0">
+            	<tr>
+               	  <td width="196" valign="top"><!-- left -->
+                    	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td background="images/layout/menu_title_bg.gif" align="center" height="30"><a style="color:#FFF;font-weight:bold" >Loại sản phẩm</a></td>
+                       	  </tr>
+                              <tr>
+                                <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Điện thoại</a></td>
+                              </tr>
+                              <tr>
+                                <td height="1"></td>
+                              </tr>
+                              <tr>
+                                <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Máy tính bảng</a></td>
+                              </tr>
+                              <tr>
+                                <td height="1"></td>
+                              </tr>
+                              <tr>
+                                <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/>  Laptop</a><td>
+                              </tr>
+                             <tr>
+                                <td height="1"></td>
+                          </tr>
+                              <tr>
+                                <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Tivi</a></td>
+                              </tr>
+                              <tr>
+                                <td height="1"></td>
+                              </tr>
+                              <tr>
+                                <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Ampli</a></td>
+                              </tr>
+                              <tr>
+                                <td height="1"></td>
+                              </tr>
+                              <tr>
+                                <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Loa</a></td>
+                              </tr>
+                      </table>
+                      
+                      <table border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                          <td height="10"></td> <!-- trắng -->
+                      </tr>
+                        <tr>
+                          <td width="196"   background="images/layout/menu_title_bg.gif" align="center" height="30" stl><a style="color:#FFF;font-weight:bold" >SP Khuyến mãi</a></td>
+                        </tr>
+                       
+                      
+                        <tr>
+                          <td width="196" height="190" align="center">
+                            <div class="oidoioi">
+
+                            <marquee direction="up" height="190">
+                              <!-- <div style="color: red;font-weight: bold;" align="center">LENOVO Yoga 500 </div>
+                               <div style="text-align:center" align="center"><img src="images/pro_img/laptops/Yoga 500 14HIW3.png" height="120"/></div>
+                          <div  align="center">13.990.000</div><div>11.169.000</div> -->
+                          <table width="100%" border="0">
+                            <tr>
+                              <td colspan="2" style="color: red;font-weight: bold;" align="center">LENOVO Yoga 500</td>
+                              <Td height="3"></Td><!--khoảng trắng-->
+
+                            </tr>
+                            <tr>
+                              <td  colspan="2" align="center"><a><img src="images/pro_img/laptops/Yoga 500 14HIW3.png" width="90" height="90"></a></td>    
+                              <Td height="3"></Td> <!--khoảng trắng-->
+                            </tr>
+                            <tr>
+                              <td align="center" style="color: red;">11.169.000</td>   
+                              <td style="color: #847676;text-decoration: line-through;">13.990.000</td>
+
+                            </tr>
+                          </table>
+                              <!-- <div style="clear:both" align="center"></div><hr color="#6da6b1" />
+                                <div class="pro_title">BOSTON AUDIO PA-1400 </div>
+                               <div style="text-align:center" align="center"><img src="images/pro_img/amplies/BOSTON AUDIO PA-14001.png" height="120" /></div>
+                          <div  align="center">8.790.000</div><div >6.690.000</div>
+                              <div style="clear:both" align="center"></div> -->
+                              <hr color="#6da6b1" />
+                              <table width="100%" border="0">
+                                <tr>
+                                  <td colspan="2" style="color: red;font-weight: bold;" align="center">BOSTON AUDIO PA-1400</td>
+                                  <Td height="3"></Td>
+                                </tr>
+                                <tr>
+                                  <td  colspan="2" align="center"><a><img src="images/pro_img/amplies/BOSTON AUDIO PA-14001.png" width="90" height="90"></a></td>                             </tr>
+                                <tr>
+                                  <td align="center" style="color: red;">6.690.000</td>   
+                                  <td style="color: #847676;text-decoration: line-through;">8.790.000</td>
+    
+                                </tr>
+                              </table>
+                            
+                               </marquee>
+</div>
+                          </td>
+
+                          </tr>
+                        </tr>
+                      </table>
+
+                    </td>
+                  <td width="24"></td> <!-- trắng -->
+                    <td width="560" valign="top" ><!-- center -->    
+              <!--------------------------------------------------------------------->
+                    <table border="0" width="100%">
+            <tr>
+            <td colspan="2" align="center"><b>THÔNG TIN ĐĂNG KÝ THÀNH VIÊN:</b></td></tr>
+            <tr><td width="150">Họ tên</td>
+           	<td><?php echo $_POST['nmHoTen']; ?></td>
+            </tr>
+            <tr><td>Ngày Sinh</td>
+           	<td><?php echo $_POST['nmNgaySinh']; ?></td>
+            </tr>
+            <tr><td>Giới tính</td>
+           	<td><?php 
+             if(isset($_POST['nmGioiTinh']))
+             {  
+                   foreach ($_POST['nmGioiTinh'] as $x){	
+                       echo $x ;
+                    }
+                  }
+               ?></td>
+           </tr> 
+            <tr><td>Địa chỉ email</td>
+           	<td><?php echo $_POST['nmEmail']; ?></td></tr>
+            <tr><td>Hình ảnh</td>
+           	<td><?php echo $_FILES['nmHinhAnh']['name']; ?></td>
+            </tr>
+            
+            <tr><td>Địa chỉ</td>
+           	<td><?php echo $_POST['nmDiachi']; ?></td></tr>
+            <tr><td>Điện thoại</td>
+           	<td><?php echo $_POST['nmDienThoai']; ?></td></tr>
+            </td>
+            </tr>
+            <tr><td>Câu hỏi bí mật</td>
+           	<td><?php echo $_POST['nmCauhoi']; ?></td></tr>
+               <tr><td>Địa chỉ</td>
+           	<td><?php echo $_POST['nmCauTL']; ?></td></tr>
+            <tr><td>Bạn đang quan tâm đến nhóm sản phẩm</td>
+           	<td><?php
+            // if(isset($_POST['nmFunctions']))
+            // {
+            // foreach ($_POST['nmFunctions'] as $x)
+            //  {	
+            //     	echo $x .";";
+            //  }
+// if(in_array('Nghe nhạc', $_POST['nmFunctions']))
+// {
+//   echo "Nghe nhạc was checked!";
+// }
+// if(in_array('Bluetooth', $_POST['nmFunctions']))
+// {
+//   echo "Bluetooth was checked!";
+// }
+ if(isset($_POST['nmFunctions']))
+            {
+              $TONTAI = $_POST['nmFunctions'];
+            foreach ($TONTAI as $x)
+             {	
+                	echo $x ."; ";
+             }       
+            }
+            else
+            {
+              echo " ko co gi";
+            }
+
+            // foreach ($_POST['nmFunctions'] as $x)
+            //  {	
+            //     	echo $x ."; ";
+            //  }       
+       
+              ?></td>
+            </tr> 
+           
+            </table>
+          <!-- ...................................... -->
+       </td>
+              <td width="24"></td> 
+                    <!-- trắng -->
+              <td width="196" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0"> <!--right -->
+                    <tr>
+                      <td background="images/layout/menu_title_bg.gif" align="center"  height="30"><a style="color:#FFF;font-weight:bold">Hãng sản xuất</a></td>
+                 </tr>
+                    <tr>
+                      <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Apple</a></td>
+                    </tr>
+                    <tr>
+                      <td height="1"></td>
+                    </tr>
+                    <tr>
+                      <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Nokia</a></td>
+                    </tr>
+                    <tr>
+                      <td height="1"></td>
+                    </tr>
+                    <tr>
+                      <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Samsung</a></td>
+                    </tr>
+                   <tr>
+                      <td height="1"></td>
+                </tr>
+                    <tr>
+                      <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> LG</a></td>
+                    </tr>
+                    <tr>
+                      <td height="1"></td>
+                    </tr>
+                    <tr>
+                      <td bgcolor="#dad0d0" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Sharp</a></td>
+                    </tr>
+                    <tr>
+                      <td height="1"></td>
+                    </tr>
+                    <tr>
+                      <td bgcolor="#eee6e6" width="220" height="30" ><a href="#" style="text-decoration:none;color:#676d77">&nbsp;&nbsp;<img src="images/layout/checked.png"/> Toshiba</a></td>
+                    </tr>
+                  </table>   
+                                   <!-- right #6da6b1 -->
+                                   <table border="0" weight="9" cellspacing="0" cellpadding="0" valign="top">
+                                    <tr>
+                                      <td height="10"></td> <!-- trắng -->
+                                  </tr>
+                                    <tr>
+                                     <td width="196"  background="images/layout/menu_title_bg.gif" align="center" height="30"><a style="color:#FFF;font-weight:bold" >Quảng cáo</a></td>
+                                    </tr>
+                                    <tr>
+                                     <!--  <td align="center"><img src="images/news/km.jpg" width="190"></td> -->
+                                   <td align="center">  <div class="oidoioi"> <img src="images/news/km.jpg" width="190"> </div></td>
+                                    </tr>
+                                  </table>
+                </td>
+                </tr>
+                 </table>
+        
+      </td>
+    </tr>
+    <tr> <!-- trắng -->
+    	<td height="450"></td>
+    </tr>
+    
+    <tr> <!-- footer -->
+    	<td background="images/layout/footer_bg.gif" height="60" witdh="5" align="center" valign="bottom" style="color:#847676;" >
+  
+    Copyright&copy; Siêu thị điện máy H-T
+    <br>
+    Địa chỉ: 180 Cao Lỗ Phường 4 Quận 8 Tp.HCM
+    <br>
+    Điện thoại: 0909123456
+      </td>
+</table>
+
+</body>
+</html>
